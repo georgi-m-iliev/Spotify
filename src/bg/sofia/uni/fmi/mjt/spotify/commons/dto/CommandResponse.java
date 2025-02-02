15897e9ba17f82428b32c2ed6033bece461b6b9b
Georgi Iliev <georgi.iliev533@outlook.com>
2025-02-02 15:59:29 +0200

    Reworked CommandResponse class

--- src/bg/sofia/uni/fmi/mjt/spotify/commons/dto/CommandResponse.java
+++ src/bg/sofia/uni/fmi/mjt/spotify/commons/dto/CommandResponse.java
@@ -2,20 +2,67 @@ package bg.sofia.uni.fmi.mjt.spotify.commons.dto;
 
 import java.util.List;
 
-public record   CommandResponse(String status, String message, AccessKey accessKey, List<Song> data) {
-    public CommandResponse(String status, String message) {
-        this(status, message, null, null);
+public class CommandResponse {
+    private final String status;
+    private final String message;
+    private final AccessKey accessKey;
+    private final List<Song> data;
+
+    private CommandResponse(Builder builder) {
+        this.status = builder.status;
+        this.message = builder.message;
+        this.accessKey = builder.accessKey;
+        this.data = builder.data;
     }
 
-    public static CommandResponse of(String status, String message) {
-        return new CommandResponse(status, message);
+    public String status() {
+        return status;
     }
 
-    public static CommandResponse of(String status, String message, AccessKey accessKey) {
-        return new CommandResponse(status, message, accessKey, null);
+    public String message() {
+        return message;
     }
 
-    public static CommandResponse of(String status, String message, List<Song> data) {
-        return new CommandResponse(status, message, null, data);
+    public AccessKey accessKey() {
+        return accessKey;
+    }
+
+    public List<Song> data() {
+        return data;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String status;
+        private String message;
+        private AccessKey accessKey;
+        private List<Song> data;
+
+        public Builder status(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder accessKey(AccessKey accessKey) {
+            this.accessKey = accessKey;
+            return this;
+        }
+
+        public Builder data(List<Song> data) {
+            this.data = data;
+            return this;
+        }
+
+        public CommandResponse build() {
+            return new CommandResponse(this);
+        }
     }
 }
--- src/bg/sofia/uni/fmi/mjt/spotify/server/command/CommandExecutor.java
+++ src/bg/sofia/uni/fmi/mjt/spotify/server/command/CommandExecutor.java
@@ -42,7 +42,10 @@ public class CommandExecutor {
 
     private CommandResponse register(String[] args) {
         if (args.length != CommandType.REGISTER.getArgumentsCount()) {
-            return CommandResponse.of("OK", "register expects 2 arguments.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("register expects 2 arguments.")
+                    .build();
         }
 
         String email = args[0];
@@ -50,64 +53,107 @@ public class CommandExecutor {
 
         users.addUser(User.of(email, password));
 
-        return CommandResponse.of("OK", "User registered successfully.");
+        return CommandResponse.builder()
+                .status("OK")
+                .message("User registered successfully.")
+                .build();
     }
 
     private CommandResponse login(String[] args) {
         if (args.length != CommandType.LOGIN.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "login expects 2 arguments.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("login expects 2 arguments.")
+                    .build();
         }
 
         String email = args[0];
         String password = args[1];
         User user = users.getUser(email);
         if (user == null) {
-            return CommandResponse.of("ERROR", "Invalid email, user not found.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("User with this email was not found.")
+                    .build();
         }
 
         if (loggedInUsers.contains(user)) {
-            return CommandResponse.of("ERROR", "User is already logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("User is already logged in.")
+                    .build();
         }
 
         if (user.validatePassword(password)) {
             loggedInUsers.add(user);
-            return CommandResponse.of("OK", "User logged in successfully.", user.getAccessKey());
+            return CommandResponse.builder()
+                    .status("OK")
+                    .message("User logged in successfully.")
+                    .accessKey(user.getAccessKey())
+                    .build();
         }
-        return CommandResponse.of("ERROR", "Invalid email/password.");
+        return CommandResponse.builder()
+                .status("ERROR")
+                .message("Invalid password.")
+                .build();
     }
 
     private CommandResponse logout(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length != CommandType.LOGOUT.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "logout expects 0 arguments.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("logout expects 0 arguments.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         if (!loggedInUsers.contains(user)) {
-            return CommandResponse.of("ERROR", "User is not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("User is not logged in.")
+                    .build();
         }
 
         loggedInUsers.remove(user);
-        return CommandResponse.of("OK", "User logged out successfully.");
+        return CommandResponse.builder()
+                .status("OK")
+                .message("User logged out successfully.")
+                .build();
     }
 
     private CommandResponse search(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length < CommandType.SEARCH.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "search expects at least 1 argument.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("search expects at least 1 argument.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         List<Song> results = songs.stream()
@@ -121,41 +167,69 @@ public class CommandExecutor {
                     return false;
                 })
                 .toList();
-        return CommandResponse.of("OK", String.format("Search has returned %s results", results.size()), results);
+        return CommandResponse.builder()
+                .status("OK")
+                .message(String.format("Search has returned %s songs.", results.size()))
+                .data(results)
+                .build();
     }
 
     private CommandResponse createPlaylist(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length != CommandType.CREATE_PLAYLIST.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "create-playlist expects 1 argument.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("create-playlist expects 1 argument.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         if (user.getPlaylists().stream().anyMatch(playlist -> playlist.name().equals(args[0]))) {
-            return CommandResponse.of("ERROR", "Playlist with the same name already exists.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Playlist with this name already exists.")
+                    .build();
         }
 
         user.getPlaylists().add(Playlist.of(args[0]));
-        return CommandResponse.of("OK", String.format("Playlist %s created successfully.", args[0]));
+        return CommandResponse.builder()
+                .status("OK")
+                .message("Playlist created successfully.")
+                .build();
     }
 
     private CommandResponse addSongTo(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length != CommandType.ADD_SONG_TO.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "add-song-to expects 2 argument.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("add-song-to expects 2 arguments.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         Playlist playlist = user.getPlaylists().stream()
@@ -163,14 +237,20 @@ public class CommandExecutor {
                 .findFirst()
                 .orElse(null);
         if (playlist == null) {
-            return CommandResponse.of("ERROR", "Playlist with this name doesn't exists.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Playlist with this name doesn't exists.")
+                    .build();
         }
 
         int songIndex;
         try {
             songIndex = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            return CommandResponse.of("ERROR", "Invalid song index.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid song index.")
+                    .build();
         }
 
         Song toAdd = songs.stream()
@@ -178,28 +258,46 @@ public class CommandExecutor {
                 .findFirst()
                 .orElse(null);
         if (toAdd == null) {
-            return CommandResponse.of("ERROR", "Song with this index doesn't exists or is already added.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Song with this index doesn't exists.")
+                    .build();
         }
 
         if (playlist.songs().contains(toAdd)) {
-            return CommandResponse.of("ERROR", "Song is already added to the playlist.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Song is already in the playlist.")
+                    .build();
         }
 
         playlist.songs().add(toAdd);
-        return CommandResponse.of("OK", "Song successfully added to playlist.");
+        return CommandResponse.builder()
+                .status("OK")
+                .message("Song added successfully.")
+                .build();
     }
 
     private CommandResponse showPlaylist(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length != CommandType.SHOW_PLAYLIST.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "show-playlist expects 1 argument.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("show-playlist expects 1 argument.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         Playlist playlist = user.getPlaylists().stream()
@@ -207,33 +305,49 @@ public class CommandExecutor {
                 .findFirst()
                 .orElse(null);
         if (playlist == null) {
-            return CommandResponse.of("ERROR", "Playlist with this name doesn't exists.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Playlist with this name doesn't exists.")
+                    .build();
         }
 
-        return CommandResponse.of(
-            "OK",
-            String.format("Playlist %s contains %s song(s).", playlist.name(), playlist.songs().size()),
-            playlist.songs());
+        return CommandResponse.builder()
+                .status("OK")
+                .message(String.format("Playlist %s contains %s song(s).", playlist.name(), playlist.songs().size()))
+                .data(playlist.songs())
+                .build();
     }
 
     public CommandResponse play(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length != CommandType.PLAY.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "play expects 1 argument.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("play expects 1 argument.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         int songIndex;
         try {
             songIndex = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            return CommandResponse.of("ERROR", "Invalid song index.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid song index.")
+                    .build();
         }
 
         Song song = songs.stream()
@@ -241,37 +355,59 @@ public class CommandExecutor {
                 .findFirst()
                 .orElse(null);
         if (song == null) {
-            return CommandResponse.of("ERROR", "Song with this index doesn't exists.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Song with this index doesn't exists.")
+                    .build();
         }
 
         // TODO: Implement playing a song
 
-        return CommandResponse.of("OK", String.format("Playing %s by %s", song.name(), song.artist()));
+        return CommandResponse.builder()
+                .status("OK")
+                .message(String.format("Playing %s by %s", song.name(), song.artist()))
+                .build();
     }
 
     public CommandResponse top(String[] args, AccessKey accessKey) {
         if (accessKey == null) {
-            return CommandResponse.of("ERROR", "You are not logged in.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("You are not logged in.")
+                    .build();
         }
         if (args.length != CommandType.TOP.getArgumentsCount()) {
-            return CommandResponse.of("ERROR", "top expects 1 argument.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("top expects 1 argument.")
+                    .build();
         }
 
         User user = users.getUser(accessKey.username());
         if (user.isAccessKeyValid(accessKey)) {
-            return CommandResponse.of("ERROR", "Invalid access key.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid access key.")
+                    .build();
         }
 
         int topCount;
         try {
             topCount = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            return CommandResponse.of("ERROR", "Invalid top count.");
+            return CommandResponse.builder()
+                    .status("ERROR")
+                    .message("Invalid top count.")
+                    .build();
         }
 
         // TODO: Implement top mechanic
 
-        return CommandResponse.of("OK", "Message", List.of());
+        return CommandResponse.builder()
+                .status("OK")
+                .message("Top songs.")
+                .data(List.of())
+                .build();
     }
 
 }
