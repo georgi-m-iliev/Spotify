bf9862e67baf0b876cbdd64206d14a125e05d182
Georgi Iliev <georgi.iliev533@outlook.com>
2025-02-06 00:05:50 +0200

    Added logger

---
+++ src/bg/sofia/uni/fmi/mjt/spotify/commons/logger/SpotifyLogger.java
@@ -0,0 +1,50 @@
+package bg.sofia.uni.fmi.mjt.spotify.commons.logger;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
+
+
+public class SpotifyLogger {
+    private static final String BASE_PACKAGE = "bg.sofia.uni.fmi.mjt.spotify.";
+    private static final Logger logger = Logger.getLogger("SpotifyLogger");
+
+    static {
+        try {
+            LogManager.getLogManager().reset();
+            logger.setLevel(Level.ALL);
+
+            FileHandler fileHandler = new FileHandler("app.log", true);
+            fileHandler.setLevel(Level.ALL);
+            fileHandler.setFormatter(
+                new Formatter() {
+                    @Override
+                    public String format(LogRecord record) {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+                        String side = "";
+                        if (record.getSourceClassName().contains("server")) {
+                            side = "Server";
+                        } else if (record.getSourceClassName().contains("client")) {
+                            side = "Client";
+                        }
+                        return String.format("[%s] [%s] [%s] %s - %s%n",
+                            dateFormat.format(new Date(record.getMillis())),
+                            side,
+                            record.getLevel(),
+                            record.getSourceClassName().replace(BASE_PACKAGE, ""),
+                            record.getMessage());
+                    }
+                });
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            System.err.println("Failed to initialize logger: " + e.getMessage());
+        }
+    }
+
+    private SpotifyLogger() {}
+
+    public static Logger getLogger() {
+        return logger;
+    }
+}
