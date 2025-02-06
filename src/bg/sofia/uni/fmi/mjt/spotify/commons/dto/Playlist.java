f1f9f102dea14dca9794e824309010e9163c21fa
Georgi Iliev <georgi.iliev533@outlook.com>
2025-02-06 10:52:15 +0200

    Fix issue with unmodifiable collection

--- src/bg/sofia/uni/fmi/mjt/spotify/commons/dto/Playlist.java
+++ src/bg/sofia/uni/fmi/mjt/spotify/commons/dto/Playlist.java
@@ -2,15 +2,21 @@ package bg.sofia.uni.fmi.mjt.spotify.commons.dto;
 
 import bg.sofia.uni.fmi.mjt.spotify.commons.json.adapters.PlaylistAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public record Playlist(UUID id, String name, List<Song> songs) {
+
+    public Playlist(String name) {
+        this(UUID.randomUUID(), name, new ArrayList<>());
+    }
+
     public Playlist(String name, List<Song> songs) {
         this(UUID.randomUUID(), name, songs);
     }
 
     public static Playlist of(String name) {
-        return new Playlist(UUID.randomUUID(), name, List.of());
+        return new Playlist(name);
     }
 }
